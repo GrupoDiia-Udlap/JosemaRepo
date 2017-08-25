4e54e391442e724ffc3db89d7fcf5bab5506b753
Josema0 <cosemine.josema@gmail.com>
2017-08-25 11:06:15 -0500

    New table (nodes)

---
+++ CSTest/src/main/scala/model/Nodo.scala
@@ -0,0 +1,28 @@
+package model
+
+import java.util.Date
+
+import com.datastax.spark.connector.mapper.DefaultColumnMapper
+
+/**
+  * Created by José Manuel Jiménez Ávila on 24/08/2017.
+  */
+class Nodo extends Serializable {
+
+  var idnodo: String = _
+  //var date: Date = _
+  var nodeattrs: Map[String, String] = Map()
+
+
+  def this(idnodo: String, graphAttrs: (String, String)*) {
+    this
+    this.idnodo = idnodo
+    //this.date = date
+    this.nodeattrs = graphAttrs.toMap
+  }
+
+}
+
+object Nodo {
+  implicit object Mapper extends DefaultColumnMapper[Nodo]
+}
--- CSTest/src/test/scala/controller/CassandraTest.scala
+++ CSTest/src/test/scala/controller/CassandraTest.scala
@@ -6,7 +6,7 @@ package controller
 
 import java.io.{BufferedWriter, File, FileWriter}
 
-import model.Interaccion
+import model.{Interaccion, Nodo}
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
@@ -40,7 +40,9 @@ class CassandraTest extends FunSuite {
       " 'replication_factor':1};",
     "CREATE TABLE IF NOT EXISTS diia.Interacciones ( idInteraccion text PRIMARY KEY, idOrigen text, tipoOrigen " +
       "text, idDestino text, tipoDestino text, tipoInteraccion text, valorInteraccion text, idPrecedente text, " +
-      "idCurso text, fecha text, plataforma text );")
+      "idCurso text, fecha text, plataforma text );",
+    "CREATE TABLE IF NOT EXISTS diia.Nodos ( idnodo text PRIMARY KEY, nodeAttrs map<text, text> );",
+    "TRUNCATE TABLE diia.Nodos;")
 
     // Extracting data from Cassandra into RDD
     val data: RDD[Interaccion] = cassandra.readDataFromCassandra("diia", "interacciones")
@@ -52,14 +54,14 @@ class CassandraTest extends FunSuite {
     var graphs: Array[(String, Graph[String, Interaccion])] = Array()
 
     // For each 'curso' do...
-    grouped.foreach((group: (String, Iterable[Interaccion])) => {
+    grouped.foreach((curso: (String, Iterable[Interaccion])) => {
       // Source
-      val origenes: Seq[(VertexId, String)] = group._2.groupBy(
+      val origenes: Seq[(VertexId, String)] = curso._2.groupBy(
         interaccion => interaccion.idorigen.toLong).
         toSeq.map(f => (f._1, f._1.toString))
 
       // Destination
-      val destinos: Seq[(VertexId, String)] = group._2.groupBy(
+      val destinos: Seq[(VertexId, String)] = curso._2.groupBy(
         interaccion => interaccion.iddestino.toLong).
         toSeq.map(f => (f._1, f._1.toString))
 
@@ -67,16 +69,18 @@ class CassandraTest extends FunSuite {
       val nodos: RDD[(VertexId, String)] = sc.parallelize(origenes.union(destinos).distinct)
 
       // Edges
-      val aristas: RDD[Edge[Interaccion]] = sc.parallelize(group._2.map(
+      val aristas: RDD[Edge[Interaccion]] = sc.parallelize(curso._2.map(
         interaccion => Edge(
           interaccion.idorigen.toLong, interaccion.iddestino.toLong, interaccion)).toSeq)
 
       // Graph creation
       val graph: Graph[String, Interaccion] = Graph(nodos, aristas)
 
-      graphs +:= (group._1, graph)
+      //val graph: Graph[String, Interaccion] = Graph.fromEdges(aristas, defaultValue = "1")
 
-      println(s"Grupo ${group._1} -> ${graph.numVertices} vértices -> ${graph.numEdges} aristas")
+      graphs +:= (curso._1, graph)
+
+      println(s"Grupo ${curso._1} -> ${graph.numVertices} vértices -> ${graph.numEdges} aristas")
 
 
       // Triplets
@@ -89,7 +93,8 @@ class CassandraTest extends FunSuite {
 
 
       // PageRank
-      val pagerank: VertexRDD[Double] = graph.pageRank(0.001).vertices
+      /*
+      val pagerank: VertexRDD[Double] = graph.pageRank(0.85).vertices
 
       pagerank.join(graph.vertices).sortBy(_._2._1, ascending = false).take(5).foreach(
         x => println(s" ${x._1} -> ${x._2._1}")
@@ -98,6 +103,7 @@ class CassandraTest extends FunSuite {
       // Joining results
       val holi: RDD[(VertexId, (String, Double))] = graph.vertices.join(pagerank)
 
+      */
       /*
       Graph(holi, aristas).triplets.
         sortBy(_.srcAttr._2, ascending = false).
@@ -126,9 +132,23 @@ class CassandraTest extends FunSuite {
     some.saveToCassandra("diia","interacciones")
     */
 
-    // Saving graph to textfile
     val graphToSave = graphs.head
 
+    // Saving new table in Cassandra
+    val pageranked: VertexRDD[Double] = graphToSave._2.pageRank(0.085).vertices
+    val indegrees: VertexRDD[PartitionID] = graphToSave._2.inDegrees
+    val outdegrees: VertexRDD[PartitionID] = graphToSave._2.outDegrees
+
+    val joined: RDD[(VertexId, (Double, (PartitionID, PartitionID)))] = pageranked.join(indegrees.join(outdegrees))
+
+    val algo: RDD[Nodo] = joined.map(f => {
+      new Nodo(f._1.toString, ("pagerank", f._2._1.toString), ("indegree", f._2._2._1.toString), ("outdegree", f._2._2._2.toString))
+    })
+
+    algo.saveToCassandra("diia", "nodos")
+
+    /*
+    // Saving graph to textfile
     val nodesFile = new File(graphToSave._1+"_nodes.csv")
     val nodesBw = new BufferedWriter(new FileWriter(nodesFile))
 
@@ -151,6 +171,8 @@ class CassandraTest extends FunSuite {
       }
     )
     edgesBw.close()
+
+    */
 
     // Closing cassandra cluster and spark context
     cassandra.session.getCluster.close()
